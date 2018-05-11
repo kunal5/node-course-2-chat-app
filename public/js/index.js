75d2b2308ad7b6ba1686f7da7c9151a56cdd43cc
Kunal Mahajan <kunalvineeta5@gmail.com>
2018-05-12 00:29:32 +0530

    Add newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,26 @@
+var socket=io();
+socket.on('connect',function (){
+  console.log('Connected to Server');
+
+  socket.emit('createMessage',{
+    to:'jen@example.com',
+    text:'Hey, This is Andrew'
+  });
+
+  // socket.emit('createEmail',{
+  //   to:'jen@example.com',
+  //   text:'Hey, This is Andrew'
+  // });
+});
+
+socket.on('disconnect',function (){
+  console.log('Disconnected from server');
+});
+
+socket.on('newMessage',function (message){
+  console.log('New Message',message);
+});
+
+// socket.on('newEmail',function (email){
+//   console.log('New Email',email);
+// });
