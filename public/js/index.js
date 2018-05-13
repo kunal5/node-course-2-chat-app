d48a5af5f0a3c5277fd46be2a43677ea40ed0407
Kunal Mahajan <kunalvineeta5@gmail.com>
2018-05-13 18:56:04 +0530

    Add form for messages and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,6 @@
 var socket=io();
 socket.on('connect',function (){
   console.log('Connected to Server');
-
-  // socket.emit('createMessage',{
-  //   from:'Andrew',
-  //   text:'Yup, that works for me'
-  // });
-
-
-
-  // socket.emit('createEmail',{
-  //   to:'jen@example.com',
-  //   text:'Hey, This is Andrew'
-  // });
 });
 
 socket.on('disconnect',function (){
@@ -21,8 +9,23 @@ socket.on('disconnect',function (){
 
 socket.on('newMessage',function (message){
   console.log('New Message',message);
+  var li=jQuery('<li></li>');
+  li.text(message.from+": "+message.text);
+  jQuery('#messages').append(li);
 });
-
-// socket.on('newEmail',function (email){
-//   console.log('New Email',email);
+// socket.emit('createMessage',{
+//   from:'Frank',
+//   text:'Hi'
+// },function(data){
+//   console.log(data);
 // });
+
+jQuery('#message-form').on('submit',function(e){
+  e.preventDefault();
+  socket.emit('createMessage',{
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  },function(){
+
+  });
+});
--- server/server.js
+++ server/server.js
@@ -16,28 +16,17 @@ io.on('connection',(socket)=>{
 
   socket.broadcast.emit('newMessage',generateMessage('Admin','New user joined'));
 
-  socket.on('createMessage',(message)=>{
+  socket.on('createMessage',(message,callback)=>{
     console.log('createMessage',message);
 
     io.emit('newMessage',generateMessage(message.from,message.text));
-
+    callback('This is from the server');
     // socket.broadcast.emit('newMessage',{
     //   from: message.from,
     //   text: message.text,
     //   createAt:new Date().getTime()
     // })
   });
-
-
-  // socket.emit('newEmail',{
-  //   from: 'mike@example.com',
-  //   text: 'Hey. What is going on.',
-  //   createAt: 123
-  // });
-  //
-  // socket.on('createEmail',(newEmail)=>{
-  //   console.log('createEmail',newEmail);
-  // });
 
   socket.on('disconnect',()=>{
     console.log('User was disconnected');
