cabcfd093663d7f0a78e955bcb5cd366cd7ed22d
Kunal Mahajan <kunalvineeta5@gmail.com>
2018-05-19 03:12:06 +0530

    Add mustache.js for message templates

--- server/utils/message.js
+++ server/utils/message.js
@@ -3,7 +3,7 @@ var generateMessage=(from,text)=>{
   return {
     from,
     text,
-    createAt:moment().valueOf()
+    createdAt:moment().valueOf()
   };
 };
 
@@ -11,7 +11,7 @@ var generateLocationMessage=(from,latitude,longitude)=>{
   return {
     from,
     url:'https://www.google.com/maps?q='+latitude+','+longitude,
-    createAt:moment().valueOf()
+    createdAt:moment().valueOf()
   };
 };
 module.exports= {generateMessage,generateLocationMessage};
